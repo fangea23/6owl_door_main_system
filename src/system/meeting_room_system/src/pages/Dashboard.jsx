e8118f005fb98419f187a72e3a141783e80abae9
underthetree <fangea23@gmail.com>
2026-01-22 15:53:18 +0800

    123

--- src/system/meeting_room_system/src/pages/Dashboard.jsx
+++ src/system/meeting_room_system/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate ,} from 'react-router-dom';
 import { supabase } from '../supabaseClient';
 import { useAuth } from '../AuthContext';
 import { usePermission, PermissionGuard } from '../../../../hooks/usePermission';
@@ -17,6 +17,7 @@ import {
   AlertCircle,
   LayoutList,
   LayoutGrid,
+  Shield,
   User
 } from 'lucide-react';
 
